9764719a56b84b1cdc189de4fa0b71b70ffc3538
Louay Morsi <louay.hesham@gmail.com>
2018-04-27 01:07:27 +0200

    Added checks on existing database before writing new user

--- Backend/StudentRegistrationSystem/APIs.py
+++ Backend/StudentRegistrationSystem/APIs.py
@@ -3,6 +3,8 @@ import json
 from backend.models import *
 import math
 
+success_response = { 'status': 'success' }
+
 def decode_password(numbers):
   hashed = bytes([])
   for b in numbers:
@@ -19,7 +21,29 @@ def extract_data(request):
   json_string = request.body.decode()
   return json.loads(json_string)
 
+def make_error_response(message):
+  return {
+    'status': 'error',
+    'error_message': message
+  }
+
 def register_user(request):
   data = extract_data(request)
-  hashed_password = decode_password(data['password']['words'])
-  return HttpResponse(json.dumps('of a7'))
+  username = data['username']
+  email = data['email']
+  try:
+    User.objects.get(username=username)
+    response = make_error_response('Username already exists')
+  except:
+    try:
+      User.objects.get(email=email)
+      response = make_error_response('Email already used')
+    except:
+      hashed_password = decode_password(data['password']['words'])
+      user = User()
+      user.username = username
+      user.password = hashed_password
+      user.email = email
+      user.save()
+      response = success_response
+  return HttpResponse(json.dumps(response))
