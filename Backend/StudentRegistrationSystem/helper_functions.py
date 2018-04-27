0e5d95044e09099514dffba897d8e49df7415c9c
Louay Morsi <louay.hesham@gmail.com>
2018-04-27 11:57:59 +0200

    Moved non API functions to another file

---
+++ Backend/StudentRegistrationSystem/helper_functions.py
@@ -0,0 +1,27 @@
+import json
+from backend.models import *
+import math
+
+success_response = { 'status': 'success' }
+
+def decode_password(numbers):
+  hashed = bytes([])
+  for b in numbers:
+    if b < 0:
+      b += (-2*b)
+    hashed += b.to_bytes(4, 'big')
+  text = bytes([])
+  for b in hashed:
+    i = math.floor( (b / 256) * 95 + 32)
+    text += i.to_bytes(1, 'big')
+  return text
+
+def extract_data(request):
+  json_string = request.body.decode()
+  return json.loads(json_string)
+
+def make_error_response(message):
+  return {
+    'status': 'error',
+    'error_message': message
+  }
