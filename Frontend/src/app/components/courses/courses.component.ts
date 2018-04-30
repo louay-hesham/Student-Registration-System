e066627d3de773928c507f1fd1b0beab3bdb01a6
Louay Morsi <louay.hesham@gmail.com>
2018-04-30 09:24:06 +0200

    Frontend: Added courses selection methods

--- Frontend/src/app/components/courses/courses.component.ts
+++ Frontend/src/app/components/courses/courses.component.ts
@@ -9,6 +9,8 @@ import { CommonService } from '../../services/common.service';
 })
 export class CoursesComponent implements OnInit {
 
+  private selectedCourses: any = { };
+
   constructor(private api: ApiService, private common: CommonService) { }
 
   ngOnInit() {
@@ -16,6 +18,9 @@ export class CoursesComponent implements OnInit {
       response => {
         if (response['status'] == 'success') {
           this.common.parseCourses(response['data']);
+          for(let code of this.getCoursesCodes()) {
+            this.selectedCourses[code] = false;
+          }
         }
       }
     )
@@ -23,6 +28,26 @@ export class CoursesComponent implements OnInit {
 
   private getCoursesCodes(): any {
     return Object.keys(this.common.courses);
+  }
+
+  private buttonClass(code: string): string {
+    if (this.selectedCourses[code]) {
+      return 'btn btn-danger';
+    } else {
+      return 'btn btn-primary';
+    }
+  }
+
+  private buttonText(code: string): string {
+    if (this.selectedCourses[code]) {
+      return 'Remove Selection';
+    } else {
+      return 'Select';
+    } 
+  }
+
+  private selectCourse(code: string) {
+    this.selectedCourses[code] = !this.selectedCourses[code];
   }
 
 }
